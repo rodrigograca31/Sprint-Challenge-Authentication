89dddd6e931f28272da7cd9fb57f2fcb794b283d
rodrigograca31 <rodrigograca1993@gmail.com>
2020-01-31 08:24:55 +0000

    🎨 Code Format

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", (req, res) => {
+	// implement registration
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+	// implement login
 });
 
 module.exports = router;
